5b79c4d387e0f03391a8c3981bc25c3b4ff60c74
simongrevin <simon.grevin@gmail.com>
2017-02-25 19:59:03 +0100

    add beautifulsoup, and find top 10 links of the week

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import requests
+from bs4 import BeautifulSoup
 
 headers = {'user-agent': 'topredditentrepreneur'}
 r = requests.get('https://www.reddit.com/r/Entrepreneur/top/?sort=top&t=week', headers=headers)
+
+soup = BeautifulSoup(r.text, "html.parser")
+
+for link in soup.find_all('a', class_="title")[0:10]:
+    print("################")
+    print(link.text)
+    print('https://www.reddit.com/' + link.get('href'))
